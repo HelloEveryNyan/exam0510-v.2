724ee5bea6cf54bd91c649b875ae8dad75ffb563
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-10-11 13:20:23 +0200

    1
    1

---
+++ Exam0510 v2/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void PrintNaturalNumbers(int N)
+    {
+        if (N > 1)
+        {
+            Console.Write(N + ", ");
+        }
+        else
+        {
+            Console.Write(N);
+        }
+
+        if (N > 1)
+        {
+            PrintNaturalNumbers(N - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("введите значение N: ");
+        if (int.TryParse(Console.ReadLine(), out int N))
+        {
+            Console.Write("N = " + N + " -> \"");
+            PrintNaturalNumbers(N);
+            Console.WriteLine("\"");
+        }
+    }
+}
---
+++ Exam0510 v2/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int SumOfNaturals(int M, int N)
+    {
+        if (M > N)
+        {
+            return 0;
+        }
+        else
+        {
+            return M + SumOfNaturals(M + 1, N);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (M <= N)
+        {
+            int result = SumOfNaturals(M, N);
+            Console.WriteLine($"cумма натуральных чисел от {M} до {N} = {result}");
+        }
+    }
+}
